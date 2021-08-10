87f842e0446cd4252c21f7ee90fc111c60ae3bad
Breno Xavier <brenoxav@gmail.com>
2021-08-10 13:04:15 -0400

    Create page header according to the wireframe

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import './style.css';
-
-function component() {
-  const container = document.createElement('div');
-  const header = document.createElement('h1');
-
-  header.textContent = 'Pokémon TCG';
-  container.appendChild(header);
-
-  return container;
-}
-
-document.body.appendChild(component());
+import logoImg from  './images/pokeball-logo.png'
+const logo = document.querySelector('.logo');
+logo.setAttribute('src', logoImg)
