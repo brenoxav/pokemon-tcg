87520945515b1419845000d881a038ddeef0d552
Abel Lavieri <abel.lavieri@gmail.com>
2021-08-13 15:31:56 -0400

    Refactor code for more readability

--- src/reservations.js
+++ src/reservations.js
@@ -2,6 +2,64 @@ import API from './API.js';
 
 const reservationPopup = document.querySelector('.reservations-popup');
 
+const generatePopupWindow = () => {
+  reservationPopup.innerHTML = `
+  <div class="reservation-content">
+    <img src="" alt="" class="poke-image">
+    <div class="card-info">
+      <button class="btn btn-close">Close</button>
+      <div class="name-class">
+        <h2></h2>
+        <p class="card-supertype"></p>
+        <p class="card-subtype"></p>
+      </div>
+      <div class="set-info">
+        <p class="card-number"></p>
+        <p class="card-rarity"></p>
+      </div>
+      <ul class="prices-list">
+      </ul>
+    </div>
+    <div class="reservations-container">
+      <div class="reservation-data-container">
+        <p class="reservations-count">Reservations: X</p>
+        <ul class="reservations-data"></ul>
+      </div>
+      <div class="reservation-form-container>
+
+      </div>          
+    </div>
+  </div>
+  `;
+}
+
+const populateCardInfo = (pokeCard) => {
+  const pokemonPrices = pokeCard.tcgplayer.prices;
+  document.querySelector('.poke-image').src = pokeCard.images.large;
+  document.querySelector('.poke-image').alt = pokeCard.name;
+  document.querySelector('.name-class h2').textContent = pokeCard.name;
+  document.querySelector('.card-supertype').textContent = pokeCard.supertype;
+  document.querySelector('.card-subtype').textContent = pokeCard.subtypes[0];
+  document.querySelector('.card-number').textContent = pokeCard.number;
+  document.querySelector('.card-rarity').textContent = pokeCard.rarity;
+  const pricesList = document.querySelector('.prices-list');
+  pricesList.innerHTML = `
+    <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].low}</li>
+    <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].mid}</li>
+    <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].high}</li>
+    <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].market}</li>
+  `;
+};
+
+const populateCardReservations = (reservations) => {
+  const reservationsList = document.querySelector('.reservations-data');
+  reservations.forEach(reservation => {
+    reservationsList.innerHTML += `
+      <li>${reservation.date_start} - ${reservation.date_end} by ${reservation.username}</li>
+    `;
+  });
+}
+
 const setCloseBtnListener = (closeBtn) => {
   closeBtn.addEventListener('click', () => {
     reservationPopup.classList.add('hidden');
@@ -10,57 +68,20 @@ const setCloseBtnListener = (closeBtn) => {
 };
 
 const openPopup = (cardID) => {
-  API.pokemon.getCardbyId(cardID)
-    .then((card) => {
-      const pokemonCard = card.data[0];
-      const pokemonPrices = pokemonCard.tcgplayer.prices;
-      reservationPopup
-        .innerHTML = `
-          <div class="reservation-content">
-          <img src="${pokemonCard.images.large}" alt="${pokemonCard.name}">  
-          <div class="card-info">
-          <button class="btn btn-close">Close</button>
-          <div class="name-class">
-            <h2>${pokemonCard.name}</h2>
-            <p class="card-supertype">${pokemonCard.supertype}</p>
-            <p class="card-subtype">${pokemonCard.subtypes[0]}</p>
-          </div>
-          <div class="set-info">
-            <p class="card-number">Number: ${pokemonCard.number}</p>
-            <p class="card-rarity">Rarity: ${pokemonCard.rarity} </p>
-          </div>
-          <ul>
-            <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].low}</li>
-            <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].mid}</li>
-            <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].high}</li>
-            <li>${pokemonPrices[Object.keys(pokemonPrices)[0]].market}</li>
-          </ul>
-          </div>
-          </div>
-          `;
-      reservationPopup.classList.remove('hidden');
-      API.involvement.getReservations(cardID)
-        .then((reservations) => {
-          if (!('error' in reservations)) {
-            const reservationContent = document.querySelector('.reservation-content');
-            reservationContent.innerHTML += `
-          <div class="reservations-container">
-            <div class="reservation-data-container">
-              <p class="reservations-count">Reservations: X</p>
-              <ul class="reservations-data"></ul>
-            </div>          
-          </div>
-        `;
-            const reservationsData = document.querySelector('.reservations-data');
-            reservations.forEach((reservation) => {
-              reservationsData.innerHTML += `
-            <li>${reservation.date_start} - ${reservation.date_end} by ${reservation.username}</li>
-          `;
-            });
-          }
-          setCloseBtnListener(document.querySelector('.btn-close'));
-        });
-    });
+  Promise.allSettled([
+    API.pokemon.getCardbyId(cardID),
+    API.involvement.getReservations(cardID)
+  ]).then(values => {
+    const pokemonCard = values[0].value.data[0]
+    generatePopupWindow();
+    populateCardInfo(pokemonCard);
+    reservationPopup.classList.remove('hidden');
+    setCloseBtnListener(document.querySelector('.btn-close'));
+    const reservations = values[1].value;
+    if (!("error" in reservations)){
+      populateCardReservations(reservations);
+    }
+  });
 };
 
 const setReservationListeners = () => {
